a1eade5a6697bb402f56aede49460f27b4cdb021
DESKTOP-NS8HAV3 <may_aj@outlook.com>
2023-10-18 16:48:35 +0200

    todo fix navbar button

---
+++ js/form-validation.js
@@ -0,0 +1,34 @@
+/*(function () {
+    'use strict'
+
+    var forms = document.querySelectorAll('.needs-validation');
+
+    Array.prototype.slice.call(forms)
+        .forEach(functioin (form) {
+            form.addEventListener('submit', function(event) {
+                if(!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()*/
+
+(function () {
+    'use strict'
+  
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
